d6f34ea8599cb39a8292aa8682cbd2915ba5422b
Vasilij U <vasya-1001@yandex.ru>
2023-05-11 21:13:01 +0500

    Zadacha 64  completed

---
+++ Z64/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+// Ввод числа
+
+int input(string text)
+{
+    System.Console.Write($"Введите {text}: ");
+    int value = int.Parse(System.Console.ReadLine());
+    return value;
+}
+
+// Проверка на положительность введеного числа
+
+void PolojitelnostN(int number)
+{
+    bool work = true;
+    while (work)
+    {
+        if (number > 0)
+        {
+            PrintNumbersToN(number); break;
+        }
+        work = false; System.Console.WriteLine("Неверное число!"); break;
+    }
+}
+
+// Рекурсия, натуральные числа от N до 1
+
+void PrintNumbersToN(int number)
+{
+    if (number > 0)
+    {
+        System.Console.Write($"{number} ");
+        number = number - 1;
+        PrintNumbersToN(number);
+    }
+}
+
+//
+
+int n = input("значение N");
+PolojitelnostN(n);
